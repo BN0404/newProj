c4db92e7efb7bf66c14392968c29607e5381d1b0
Bhagwati Nagarkoti <indu.ghugtyal@gmail.com>
2023-03-06 20:13:31 -0500

    final

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
 const image = document.getElementById('image')
-const start = document.querySelector('playAgain')
+const start = document.querySelector("#playAgain");
 
 // console.log(image)
-let currentQuestion = {}
-let acceptingAnswer = true
-let score = 0
-let questionCounter = 0
-let availableQuestions = []
+let currentQuestion = {};
+let acceptingAnswer = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
 
 // class Player {
 //     constructor(name = 'Player1', age, country) {
@@ -28,191 +28,166 @@ let availableQuestions = []
 
 // let country = prompt("Enter your Country:")
 
-
 // const player = new Player(name, age, country )
 
-
-
 let questions = [
-    {
-        question: "Guess the cartoon series.",
-        // show_image('babylooney.gif', '30%', '40%', 'cartoon img')
+  {
+    question: "Guess the cartoon series.",
 
-        img: "./img/babylooney.gif",
-          choice1 : "Bunnies and friends",
-           choice2 : "Baby Looney Tunes",
-           choice3 : "Baby Bunnies",
-           choice4 : "Lullaby tunes",
-            answer: 2
-    },
-    {
-        question: "Guess the cartoon.",
-        img: "./img/dexter.gif",
-        
-        choice1: "Lab doctor",
-        choice2: "Pixer the doctor",
-        choice3: "Romeo",
-        choice4: "Deter",
-        answer: 4
-        // options: ["Lab doctor", "Pixer the doctor", "Romeo", "Deter"]
-    },
-    {
-        question: "What is this cartoon's last name.",
-        img: "./img/Luigi.gif",
-        choice1: "Mario",
-        choice2: "Super",
-        choice3: "Luigi",
-        choice4: "Mario Luigi",
-        answer: 1
-        // options: ["Mario", "Super", "Luigi", "Mario Luigi"]
-        // correctAnswer: "Mario",
-    
-    },
-    {
-        question: "Who is hiding in the image?",
-        img: "./img/mickey.gif",
-        choice1: "Cinderella Place",
-        choice2: "Disney",
-        choice3: "Mouse",
-        choice4: "Mickey and friends",
-        answer: 2
-        // options: ["Cinderella Place", "Disney", "Mouse", "Mickey and friends"]
-        // correctAnswer: "Disney",
-    },
-    {
-        question: "What this character is waiting for?",
-        img: "./img/winnie.gif",
-        choice1: "Winnie",
-        choice2: "Pancake",
-        choice3: "Honey",
-        choice4: "Spaghetti with meat balls",
-        answer: 3
-        // options: ["Winnie", "Pancake", "Honey", "Spaghetti with meat balls"]
-        // correctAnswer: "Disney",
-    },
-
-
-]
+    img: "./img/babylooney.gif",
    
-const SCORE_POINTS = 100
-const MAX_QUESTIONS = 5
+    choice1: "Bunnies and friends",
+    choice2: "Baby Looney Tunes",
+    choice3: "Baby Bunnies",
+    choice4: "Lullaby tunes",
+    answer: 2,
+  },
+  {
+    question: "Guess the cartoon.",
+    img: "./img/dexter.gif",
 
-startGame = () => {
-    // questionCounter = 0
-    score = 0
-    availableQuestions = [...questions]
-    // getNewQuestion()
-    // counter =10
-    // show_image()
+    choice1: "Lab doctor",
+    choice2: "Pixer the doctor",
+    choice3: "Romeo",
+    choice4: "Dexter",
+    answer: 4,
+  
+  },
+  {
+    question: "What is this cartoon's last name.",
+    img: "./img/Luigi.gif",
+    choice1: "Mario",
+    choice2: "Super",
+    choice3: "Luigi",
+    choice4: "Mario Luigi",
+    answer: 1,
+  
+  },
+  {
+    question: "What image is hiding in the picture?",
+    img: "./img/mickey.gif",
+    choice1: "Cinderella Place",
+    choice2: "Disney",
+    choice3: "Mouse",
+    choice4: "Mickey and friends",
+    answer: 2,
    
-    // selectedChoice.parentElement.classList.add(classToApply)
-    //     setInterval(() => {
-    //         selectedChoice.parentElement.classList.remove(classToApply)
-    //         getNewQuestion()
-    //         console.log(questionCounter)
-    //         questionCounter ++
-    //         if (questionCounter === MAX_QUESTIONS && incrementScore >= 300) {
-    //             console.log("if")
-    //             alert('Yay you are the winner');
-    //         }
-    //         else if(questionCounter === MAX_QUESTIONS && incrementScore <300) {
-    //             alert('Better luck next time');
-    //         }
-            
-    //     },3000)
-}
+  },
+  {
+    question: "What this character is waiting for?",
+    img: "./img/winnie.gif",
+    choice1: "Winnie",
+    choice2: "Pancake",
+    choice3: "Honey",
+    choice4: "Spaghetti with meat balls",
+    answer: 3,
+
+  },
+];
+
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 5;
+
+const startGame = () => {
+  // window.location.reload()
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+};
+
+start.addEventListener("click", startGame)
+
 
 //getting new question
 getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS)
-        {
-            // localStorage.setItem('mostRecentScore', score)
-            // return window.location.assign('/end.html')
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        // localStorage.setItem('mostRecentScore', score)
+        // return window.location.assign('/end.html')
 
-            return
-        } 
-        
-    console.log('testing')
-//tracks the progress of the questions
-    questionCounter++
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
-    
-    //Keeping track of the questions. Which questions we are at?
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[ questionsIndex ]
-    console.log(currentQuestion);
-    question.textContent = currentQuestion.question
-    
-    choices.forEach(choice => {
-        const number = choice.dataset[ 'number' ]
-        choice.innerText = currentQuestion[ 'choice' + number ]
-    })
-    
-    availableQuestions.splice(questionsIndex, 1)
-    image.setAttribute('src', "./"+ currentQuestion.img)
-    // video.setAttribute('src', currentQuestion.video)
-    
-    acceptingAnswer = true
-    // startGame()
-}
+        return;
+    }
 
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if (!acceptingAnswer) return
-        
-        acceptingAnswer = false
-        const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset[ 'number' ]
+      console.log(questionCounter)
+    if (questionCounter === MAX_QUESTIONS - 1) {
+        // start.addEventListener("click", startGame) 
+            startGame();
+    }
     
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
-    
-        if (classToApply === 'correct') {
-    
-            incrementScore(SCORE_POINTS)
-        }
-        selectedChoice.parentElement.classList.add(classToApply)
-        //for time 
-        setTimeout(() => {
 
-            selectedChoice.parentElement.classList.remove(classToApply)
-            
-            console.log(questionCounter, MAX_QUESTIONS, score)
-            if (questionCounter=== MAX_QUESTIONS && score >= 300) {
-    alert('Yay you are the winner');
-}
-else if(questionCounter === MAX_QUESTIONS && score <300) {
-    alert('Better luck next time');
-            }
-            getNewQuestion()
-        }, 5000)
-    })
-})
+  //tracks the progress of the questions
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  //Keeping track of the questions. Which questions we are at?
+  const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionsIndex];
+//   console.log(currentQuestion);
+  question.textContent = currentQuestion.question;
+
+    
+
+    
+    
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuestions.splice(questionsIndex, 1);
+  image.setAttribute("src", "./" + currentQuestion.img);
+  // video.setAttribute('src', currentQuestion.video)
+
+  acceptingAnswer = true;
+  // startGame()
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswer) return;
+
+    acceptingAnswer = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    let classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "correct") {
+      incrementScore(SCORE_POINTS);
+    }
+    selectedChoice.parentElement.classList.add(classToApply);
+    //for time
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+
+    //   console.log(questionCounter, MAX_QUESTIONS, score);
+      if (questionCounter === MAX_QUESTIONS && score >= 300) {
+        alert("Yay you are the winner");
+      } else if (questionCounter === MAX_QUESTIONS && score < 300) {
+        alert("Better luck next time");
+      }
+      getNewQuestion();
+    }, 3000);
+  });
+});
 incrementScore = num => {
     score += num
     scoreText.innerText = score
 }
 
-// if (questionCounter=== MAX_QUESTIONS && incrementScore >= 300) {
-//     alert('Yay you are the winner');
-// }
-// else if(questionCounter === MAX_QUESTIONS && incrementScore <300) {
-//     alert('Better luck next time');
-// }
-
-
-
 startGame()
+// start.addEventListener("click", startGame)
 // let questionCounter = 0
 
 selectedChoice.parentElement.classList.add(classToApply)
         setInterval(() => {
             selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
-            console.log(questionCounter)
+            // console.log(questionCounter)
             questionCounter ++
             if (questionCounter === MAX_QUESTIONS && incrementScore >= 300) {
-                console.log("if")
+                // console.log("if")
                 alert('Yay you are the winner');
             }
             else if(questionCounter === MAX_QUESTIONS && incrementScore <300) {
@@ -221,33 +196,3 @@ selectedChoice.parentElement.classList.add(classToApply)
             
         },3000)
         
-        // selectedChoice.parentElement.classList.add(classToApply)
-        // setInterval(() => {
-        //     selectedChoice.parentElement.classList.remove(classToApply)
-        //     getNewQuestion()
-        // },5000)
-
-
-        
-
-        // function show_image(src, width, height, alt) {
-        //     var img = document.createElement("img");
-        //     img.src = src;
-        //     img.width = width;
-        //     img.height = height;
-        //     img.alt = alt;
-        
-        //     // This next line will just add it to the <body> tag
-        //     document.body.appendChild(img);
-        // }
-
-
-
-        // countdown: function(){
-        //     startGame.counter --;
-        //     $('#counter').html(startGame.counter); 
-        //     if(startGame.counter<=0){
-        //         console.log("TIME UP!")
-        //         game.timeUp();
-        //     }
-        // },
---
+++ text.md
@@ -0,0 +1,39 @@
+# **About the project:**
+
+It’s an entertainment quiz for any age group. It has timed question and if you reach a certain level, you will be a winner. So good luck and increase your knowledge about the cartoon’s. 
+There will be the next level of this coming soon.
+
+
+
+## ** Built With: **
+
+1. ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+
+2. ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+
+3. ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+
+
+
+## ** Usage: **
+
+It helps you in increasing your knowledge about Cartoons. It helps your kid to learn patience and increases their focus. Excellent way to spend time with your kids and understand each other better. Also compete with each other at the same time. Have fun playing with it. 
+
+
+## **Author and contact:**
+
+B_Nagarkoti;
+indu.ghugtyal@gmail.com
+
+
+## **Acknowledgment**
+
+https://www.youtube.com/watch?v=5fb2aPlgoys&t=2939s
+https://www.youtube.com/watch?v=f4fB9Xg2JEY&list=WL&index=5&t=2880s
+https://www.youtube.com/watch?v=u1HyG535JY0
+https://www.markdownguide.org/cheat-sheet/#extended-syntax
+https://www.sitepoint.com/simple-javascript-quiz/
+
+
+
